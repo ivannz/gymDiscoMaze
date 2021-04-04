0159fe9572dd3ddf1510204aee1ec6e600a2e8b1
ivannz <ivannz@yandex.ru>
2021-04-04 23:04:03 +0300

    more streamlined rendering

--- gym_discomaze/render.py
+++ gym_discomaze/render.py
@@ -1,44 +1,57 @@
 import numpy as np
 
-from pyglet import gl
+from pyglet.gl import GL_QUADS
+from pyglet.graphics import vertex_list
+from gym.envs.classic_control.rendering import Viewer as _Viewer
 
-from gym.envs.classic_control.rendering import Viewer
-from gym.envs.classic_control.rendering import FilledPolygon
+
+class Viewer(_Viewer):
+    """Allow default closing sequence"""
+    def window_closed_by_user(self):
+        type(self.window).on_close(self.window)
+        super().window_closed_by_user()
+        self.window.close()
+
+
+class FilledQuadArray:
+    """Pixel array using pyglet's streamlined array drawing."""
+
+    def __init__(self, quads, colors):
+        self.quads, self.colors = quads, colors
+
+    def render(self):
+        # `quads` is a flattened array of shape `(n_quads, 4, 2)`
+        # `colors` is an array of shape `(n_quads, 3)`
+        vl = vertex_list(
+            len(self.quads) // 2, ('v2f', self.quads),
+            ('c3f', list(np.tile(self.colors, (1, 4)).flat))
+        )
+        vl.draw(GL_QUADS)
+        vl.delete()
 
 
 class Renderer:
     def __init__(self, n_row, n_col, *, pixel=(1, 1)):
         h, w = pixel
-        self.viewer = Viewer(n_col * h, n_row * w)
+        self.viewer = Viewer(n_col * w, n_row * h)
         self.viewer.set_bounds(0, n_col, 0, n_row)
 
-        pixels = []
+        # pyglet uses lower left as the origin, while we use upper left
+        self._quads = []  # create quads in numpy's order
         for i in range(n_row):
             for j in range(n_col):
-                pixel = FilledPolygon([
-                    (j+0, n_row-(i+0)), (j+0, n_row-(i+1)),
-                    (j+1, n_row-(i+1)), (j+1, n_row-(i+0)),
-                ])
-                pixels.append(pixel)
-                self.viewer.add_geom(pixel)
-        self.pixels = np.array(pixels).reshape(n_row, n_col)
+                self._quads.extend((
+                    j+0, n_row-(i+0),  # ur
+                    j+0, n_row-(i+1),  # lr
+                    j+1, n_row-(i+1),  # ll
+                    j+1, n_row-(i+0),  # ul
+                ))
 
-    @property
-    def shape(self):
-        return self.pixels.shape
-
-    def __getitem__(self, index):
-        return self.pixels[index]
+    def render(self, pixels, mode='human'):
+        self.viewer.add_onetime(
+            FilledQuadArray(self._quads, pixels.reshape(-1, 3)))
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
     def close(self):
         self.viewer.close()
         del self.viewer
-        del self.pixels
-
-    def render(self, pixels, mode='human'):
-        n_row, n_col = self.shape
-        for i in range(n_row):
-            for j in range(n_col):
-                self[i, j].set_color(*pixels[i, j])
-
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
