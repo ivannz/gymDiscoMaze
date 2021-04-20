00082f0d4edf1d20697d6de9e40c06bc6773d30e
ivannz <ivannz@yandex.ru>
2021-04-20 17:04:40 +0300

    minor bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='RandomDiscoMaze',
     description='Randomized Maze for sec.4.1 of https://arxiv.org/abs/2002.06038',
-    version='0.3',
+    version='0.4',
     license='MIT',
     packages=[
         'gym_discomaze'
